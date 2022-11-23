be53f0474556168ddec3e0cfca23f738152f15fa
hegde10122 <hegde10122@gmail.com>
2022-11-23 16:36:30 +0530

    updated
    updated

--- src/main/kotlin/helloworld.kt
+++ src/main/kotlin/helloworld.kt
@@ -1,4 +1,10 @@
 fun main(){
+    //main() is a function denoted by the keyword fun()
+
     println("Hello World !!")
+    //println is also a function
 }
+
+//Here the code (the main function) may not reside inside a class like Java. In java all methods reside inside a class.
+// Java methods equivalent to Kotlin functions loosely.
 
