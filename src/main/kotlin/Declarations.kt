10c8f83561ba9293ae3764ab2032768a97cd6b10
hegde10122 <hegde10122@gmail.com>
2022-11-23 18:52:10 +0530

    update
    update

--- src/main/kotlin/Declarations.kt
+++ src/main/kotlin/Declarations.kt
@@ -8,5 +8,23 @@ fun main(){
 
       var q : Double = 1.34
 
+      val n : Int = 44
+      val k : String = "I am rich"
+      var a  = 44.55
 
+      number = 82
 }
+
+//In the Intellij IDE, the variables z,q and a will be underlined to denote the fact that they are variables of type "var". The "val" variables
+//are not underlined. You can see number,n and k without underlines. This distinction is available in IDEs like IntelliJ only. Kotlin
+//explicitly does not underline them. Hope you get this important point.
+
+//val keyword is used to declare a constant called q (line 9) which is of type Double.
+//Then it sets the value of the constant to the decimal number 1.34
+
+//val keyword is used to declare a constant called x which is of type String (line 7).
+//Then it sets the value of the constant to the data string "kotlin"
+// = is the assignment operator
+
+
+
